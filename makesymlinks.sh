253d7f9684dc617819740925c986d50bec4a1257
Matt Gibson <contact.matt.gibson@gmail.com>
2013-07-21 15:43:41 +1000

    Fixed directory paths error

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -6,8 +6,8 @@
 
 ##### Variables
 
-dir=-/dotfiles   		#dotfiles directory
-olddir=-/dotfiles_old		#old dotfiles backup directory
+dir=~/dotfiles   		#dotfiles directory
+olddir=~/dotfiles_old		#old dotfiles backup directory
 files="bashrc vimrc vim" 		#list of files/folders to symlink in homedir
 
 #####
